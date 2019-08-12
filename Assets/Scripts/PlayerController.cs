d0fd732607e9c19e0fe57e84911b75a3f40715b8
tomabolt <thimblot@gmail.com>
2019-08-12 22:19:40 +0200

    Make ability to jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     public float turnSpeed = 5f;
     public float playerSpeed = 5f;
+    public float jumpingHeight = 100f;
     public LevelController levelController;
     public AudioSource fallingSound;
 
     Rigidbody m_Rigidbody;
+    Collider m_Collider;
     Vector3 m_Movement; 
 
     void Start() {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<Collider>();
     }
 
-    void FixedUpdate() {
+    bool isGrounded() {
+        float distanceToTheGround = m_Collider.bounds.extents.y;
+        return Physics.Raycast(transform.position, Vector3.down, distanceToTheGround + 0.1f);
+    }
+
+    void Update() {
         float horizontal = Input.GetAxis ("Horizontal");
         float vertical = Input.GetAxis ("Vertical");
         m_Movement.Set(horizontal, 0, vertical);
         m_Movement.Normalize();
         m_Rigidbody.AddForce(m_Movement * playerSpeed);
+
+        if (Input.GetKeyDown ("space") && isGrounded()) {
+            m_Rigidbody.AddForce(Vector3.up * jumpingHeight);
+        }
     }
 
     
