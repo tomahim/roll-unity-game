1f7a13018c3c773edfae2431aad16081bf285ca6
tomabolt <thimblot@gmail.com>
2019-08-15 18:38:12 +0200

    Add timer to LevelController

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -9,12 +9,22 @@ public class LevelController : MonoBehaviour
 {
     public Text scoreText;
 
+    public bool enableTimer = false;
+    public float timeLimit = 60.0f;
+    public Text timerText;
+    public AudioSource timerEndedSound;
+
     private int nbCollectedGems = 0;
     private int nbTotalGems;
+    private bool timerEndedExecuted = false;
     
     private void Start() {
         nbTotalGems = GameObject.FindGameObjectsWithTag("Gem").Length;
         updateScore();
+    }
+
+    private void Update() {
+        checkTimer();
     }
 
     public void gemRetrieved() {
@@ -27,5 +37,31 @@ public class LevelController : MonoBehaviour
 
     private void updateScore() {
         scoreText.text = Convert.ToString(nbCollectedGems) + " / " + Convert.ToString(nbTotalGems);
+    }
+
+    private void checkTimer() {
+        if (enableTimer) {
+            timeLimit -= Time.deltaTime;
+            timerText.text = Convert.ToString(Math.Round(timeLimit, 0));
+            if (timeLimit < 10.0f) {
+                timerText.color = new Color(220f/255.0f, 20f/255.0f, 60f/255.0f);
+            }
+            if (timeLimit <= 0.0f) {
+                timerEnded();
+            }
+        } else {
+            timerText.text = "";
+        }
+    }
+
+    private void timerEnded() {
+        timerText.text = "TIME'S UP !";
+        if (!timerEndedExecuted) {
+            if (!timerEndedSound.isPlaying) {
+                timerEndedSound.Play();
+            }
+            timerEndedExecuted = true;
+            StartCoroutine(LevelTransition.loadLevel());
+        }
     }
 }
