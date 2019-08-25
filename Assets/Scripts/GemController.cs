7266fe9c8782dadada1d1ceee57a35704c4a10d5
tomabolt <thimblot@gmail.com>
2019-08-25 20:25:23 +0200

    Add particle wen gem is catched

--- Assets/Scripts/GemController.cs
+++ Assets/Scripts/GemController.cs
@@ -6,10 +6,20 @@ public class GemController : MonoBehaviour
 {   
     public AudioSource pickupSound;
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            pickupSound.Play();
-            gameObject.SetActive(false);
-        }
-    }
+     private void OnTriggerEnter(Collider other) {                           
+        if (other.gameObject.CompareTag("Player")) {                        
+            pickupSound.Play();                                             
+            StartCoroutine(makeGemDisappear());                                      
+        }                                                                   
+    }                                                                       
+                                                                            
+    private IEnumerator makeGemDisappear() {
+        Debug.Log("disappear");                                
+        ParticleSystem particle = transform.GetComponent<ParticleSystem>(); 
+        particle.Play();                                                    
+        transform.Find("Cones").gameObject.SetActive(false);                
+        yield return new WaitForSeconds(0.8f);                                
+        particle.Stop();                                                    
+        Destroy(transform.gameObject);                                      
+    }                                                                       
 }
