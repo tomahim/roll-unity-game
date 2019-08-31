2bd96005c611347b890a0309cc070e3be85ff32a
tomabolt <thimblot@gmail.com>
2019-09-01 01:02:23 +0200

    Add a confirm message on start new game button

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject continueButton;
     public AudioManager audioManager;
 
+    private GameObject confirmMessage;
+    private int lastLevelPlayed;
+
     private void Start() {
-        int lastLevelPlayed = PlayerPrefs.GetInt("currentLevelNumber");
+        lastLevelPlayed = PlayerPrefs.GetInt("currentLevelNumber");
+        confirmMessage = GameObject.Find("ConfirmMessage").GetComponent<Image>().gameObject;
+        confirmMessage.SetActive(false);
         LevelTransition.currentLevelNumber = lastLevelPlayed;
         if(lastLevelPlayed == 1) {
             continueButton.SetActive(false);
         }
     }
 
-    public void newGame() {
-        LevelTransition.newGame();
+    public void newGame(bool forceNewGame) {
+        if (lastLevelPlayed > 1 && !forceNewGame) {
+            showConfirmNewGame(true);
+        } else {
+            LevelTransition.newGame();
+        }
+    }
+
+    public void showConfirmNewGame(bool isActive) {
+        confirmMessage.gameObject.SetActive(isActive);
     }
 
     public void continueGame() {
