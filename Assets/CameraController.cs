124f53b854870f8d637af7a371a8dc8fb692dfaa
tomahim <thimblot@gmail.com>
2019-09-14 18:06:04 +0200

    Fix cinemachine virtual camera glitch with a custom script

---
+++ Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+   public GameObject player;   
+   public bool isShaking = false;
+ 
+   private Vector3 offset;
+ 
+   // A measure of magnitude for the shake. Tweak based on your preference
+   public float shakeMagnitude = 0.7f;
+ 
+   // The initial position of the GameObject
+   Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start() {
+	offset = transform.position - player.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 newPosition = player.transform. position + offset;
+        if (isShaking) {
+	newPosition += (Random.insideUnitSphere * shakeMagnitude);
+        } 
+        transform.position = newPosition;
+    }
+}
